86877214f8bf35d4f15f2d28d74b1c3ce9d815ff
iyeongjun <they6687@naver.com>
2018-02-17 17:06:07 +0900

    add Home MVP

--- app/src/main/java/com/veryworks/iyeongjun/kotlinmvpsample/view/main/home/adapter/ImageRecycleradapter.kt
+++ app/src/main/java/com/veryworks/iyeongjun/kotlinmvpsample/view/main/home/adapter/ImageRecycleradapter.kt
@@ -1,6 +1,24 @@
 package com.veryworks.iyeongjun.kotlinmvpsample.view.main.home.adapter
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.veryworks.iyeongjun.kotlinmvpsample.view.main.home.adapter.holder.ImageViewHolder
+
 /**
  * Created by iyeongjun on 2018. 2. 17..
  */
-class ImageRecycleradapter
+class ImageRecycleradapter(private val context : Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    private val list = mutableListOf<>()
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+        return ImageViewHolder(context,parent)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+        (holder as? ImageViewHolder)?.onBind()
+    }
+}
--- app/src/main/java/com/veryworks/iyeongjun/kotlinmvpsample/view/main/home/adapter/holder/ImageViewHolder.kt
+++ app/src/main/java/com/veryworks/iyeongjun/kotlinmvpsample/view/main/home/adapter/holder/ImageViewHolder.kt
@@ -1,5 +1,23 @@
 package com.veryworks.iyeongjun.kotlinmvpsample.view.main.home.adapter.holder
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.veryworks.iyeongjun.kotlinmvpsample.R
+import kotlinx.android.synthetic.main.itme_image_view.view.*
+
 /**
  * Created by iyeongjun on 2018. 2. 17..
  */
+
+class ImageViewHolder(context: Context, parent : ViewGroup?) : RecyclerView.ViewHolder(LayoutInflater.from(context).inflate(R.layout.itme_image_view,parent,false)){
+    fun onBind(item: String){
+        itemView.onBind(item)
+    }
+
+    private fun View.onBind(item : String){
+        img_view.setImageResource(resources.getIdentifier(item,"drawble",context.packageName))
+    }
+}
